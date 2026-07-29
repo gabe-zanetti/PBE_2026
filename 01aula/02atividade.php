4af8c95d8f80a96a77c53951357abb65de5dfe38
GABRIELA NOGUEIRA DE CARVALHO FELIX ZANETTI <gabriela.zanetti3@portalsesisp.org.br>
2026-07-29 13:31:31 -0300

    29 de julho, 13:30, adicionando atividades

---
+++ 01aula/02atividade.php
@@ -0,0 +1,13 @@
+<?php
+    $nome="Estudande";
+    $idade= 20;
+    echo "Olá, $nome <br>";
+    echo "Você tem $idade anos";
+
+    if ($idade >=18){
+        echo "Maior de idade";
+    }
+    else {
+        echo "Menor de idade";
+    }
+?>
